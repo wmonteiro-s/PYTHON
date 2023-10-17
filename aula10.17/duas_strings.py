9426ddf85933762cdb2b7a408827b5c40a3a1f31
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-17 16:43:44 -0300

    conteúdo atualizado para utilização do ESCAPE

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,8 +1,11 @@
+# você pode ESCAPAR aspas duplas utilizando aspas simples
+# vale reforçar que isso só vale para aspas duplas, outros caracteres não
 # texto1 recebe o valor "Sou do"
 texto1 = "Sou do"
 
 # texto2 recebe o valor "back-end"
-texto2 = "back-end"
+# caracter de SCAPE é o \
+texto2 = "\"back-end\""
 
 
 # a função print, concatena e imprime as duas variaveis na tela
