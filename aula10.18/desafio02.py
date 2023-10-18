9930ff2aa5838db9fb78646553ace56f2a59551d
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-18 15:58:56 -0300

    modificando o arquivo desafio02.py

--- aula10.18/desafio02.py
+++ aula10.18/desafio02.py
@@ -1,6 +1,6 @@
-idade = input('Digite a sua idade? ')
-idade_parente = input('Digite a idade de algum parente seu: ')
-nome = input('Digite o seu nome')
+idade = int(input('Digite a sua idade? '))
+idade_parente = int(input('Digite a idade de algum parente seu: '))
+nome = input('Digite o seu nome ')
 
 print(idade * idade_parente)
 print(nome)
