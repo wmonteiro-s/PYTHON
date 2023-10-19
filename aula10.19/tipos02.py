43b28ca4fc05890b29a106167b095ffb68c34203
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-19 15:34:58 -0300

    conteúdo sobre tipos

---
+++ aula10.19/tipos02.py
@@ -0,0 +1,13 @@
+# entender tipos e mudanças de tipos dentro do python
+
+a = 50
+b = "50"
+print(type(a))
+print(type(b))
+
+idade = int(input('Qual sua idade? '))
+print(type(idade))
+
+# utilização do float: moeda, peso, temperatura, medidas, notas
+peso = float(input('Qaul seu peso? '))
+print(type(peso))
