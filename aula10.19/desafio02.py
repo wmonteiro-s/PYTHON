5b9b81c1efc8264df86ed6165ec091a7c1e476a6
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-20 15:01:29 -0300

    desafio02.py concluído

---
+++ aula10.19/desafio02.py
@@ -0,0 +1,4 @@
+altura = float(input('Informe sua altura: '))
+
+print(f'Se vc é um homem, seu peso ideal é: {(72.7 * altura) - 58}')
+print(f'Se vc é uma mulher, seu peso ideal é: {(62.1 * altura) - 44.7}')
