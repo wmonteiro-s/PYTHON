fb7efdc3f3681761780b77d7cc127a634a85c104
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-20 15:42:23 -0300

    desafios.py 01 e 02 comentados

--- aula10.19/desafio01.py
+++ aula10.19/desafio01.py
@@ -1,3 +1,6 @@
-f = float(input('Qual a temperatura em graus farenheit? '))
+# variável f recebe um input de tipo float que por sua vez recebe um valor de acordo com o usuário
+f = float(input('Qual a temperatura em graus Farenheit? '))
 
-print('A temperatura convertida em graus Celsius é',  5 * (f - 32) / 9)
+# função print para mostrar a mensagem e valores na tela
+print(f'A temperatura convertida em graus Celsius é, {5 * (f - 32) / 9}')
+# vale ressaltar o cálculo interpolado na função que tem o intuito de mostrar a temperatura convertida em graus Celsius
