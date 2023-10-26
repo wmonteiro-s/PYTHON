558b85b6ece296d524d71f96064c81adc418da8e
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-26 14:10:35 -0300

    traduzindo variáveis para o inglês

--- aula10.19/desafio02.py
+++ aula10.19/desafio02.py
@@ -1,7 +1,7 @@
 # variável altura recebe um input de tipo float que por sua vez recebe um valor de acordo com o usuário
-altura = float(input('Informe sua altura: '))
+height = float(input('Informe sua altura: '))
 
 # função print para mostrar a mensagem e valores na tela
-print(f'Se vc é um homem, seu peso ideal é: {(72.7 * altura) - 58}')
-print(f'Se vc é uma mulher, seu peso ideal é: {(62.1 * altura) - 44.7}')
+print(f'Se vc é um homem, seu peso ideal é: {(72.7 * height) - 58}')
+print(f'Se vc é uma mulher, seu peso ideal é: {(62.1 * height) - 44.7}')
 # vale ressaltar que o cálculo interpolado e o valor a ser mostrado é diferente de acordo com o gênero
--- aula10.19/desafio03.py
+++ aula10.19/desafio03.py
@@ -1,9 +1,9 @@
 # variável salário recebe um input de tipo float que por sua vez recebe um valor de acordo com o usuário 
-salario = float(input('Digite o valor do seu salário: '))
+wage = float(input('Digite o valor do seu salário: '))
 
 # o mesmo acontece com a variável percentual_reajuste
-percentual_reajuste = float(input('Digite o percentual de reajuste que queira aplicar em seu salário: '))
+percentage_adjustment = float(input('Digite o percentual de reajuste que queira aplicar em seu salário: '))
 
 # função print para mostrar a mensagem e valores na tela
-print(f'Seu antigo salário era {salario}, já seu atual salário após o reajuste é {((percentual_reajuste / 100) * salario) + salario}')
+print(f'Seu antigo salário era {wage}, já seu atual salário após o reajuste é {((percentage_adjustment / 100) * wage) + wage}')
 # vale ressaltar o cálculo interpolado na função que tem o intuito de mostrar o salário após o reajuste
