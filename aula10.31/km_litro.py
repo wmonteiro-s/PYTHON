d0ce84269247f0f1e183f831cb5d39f2a9684935
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-31 16:11:16 -0300

    criada a pasta aula10.31 com arquivo km_litro.py

---
+++ aula10.31/km_litro.py
@@ -0,0 +1,11 @@
+km = float(input("Quantos quilômetros foram percorridos? "))
+litros = float(input("Quantos litros de combustível foram gastos? "))
+
+consumo_por_km = km / litros
+
+if consumo_por_km < 8.0:
+    print("Venda o carro")
+elif consumo_por_km > 8.0 and consumo_por_km < 14.0:
+    print("Econômico")
+else:
+    print("Super econômico")
