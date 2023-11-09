95c438bffe06c521138bfea8522e8e92960a9de5
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-09 20:52:32 -0300

    8 palavras foram fatiadas para formar uma frase

---
+++ aula11.09/ambiente_virtual/atividade02.py
@@ -0,0 +1,5 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. \
+Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, \
+instrutores e empresas parceiras."
+
+print(texto[20:30], texto[31:33], texto[34:44], texto[114:125], texto[126:133], texto[76:88], texto[89:90], texto[160:171])
