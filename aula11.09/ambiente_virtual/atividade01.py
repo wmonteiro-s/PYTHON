9e5d9b9715a255e46826ce797c7191300ae4e62e
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-09 20:51:37 -0300

    10 palavras da variavel texto foram fatiadas

---
+++ aula11.09/ambiente_virtual/atividade01.py
@@ -0,0 +1,14 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. \
+Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, \
+instrutores e empresas parceiras."
+
+print(texto[0:5])
+print(texto[10:16])
+print(texto[20:30])
+print(texto[34:44])
+print(texto[49:53])
+print(texto[54:60])
+print(texto[67:74])
+print(texto[76:88])
+print(texto[91:99])
+print(texto[101:109])
