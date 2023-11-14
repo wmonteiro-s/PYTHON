d74ff6f91a23a5c6693e775a47d00598911795f3
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-14 20:01:35 -0300

    aprendendo sobre tuplas

---
+++ aula11.14/tupla.py
@@ -0,0 +1,6 @@
+# tuplas são imutáveis
+# parametro parenteses
+# obs: caso você não informe o parametro da lista ou da tupla, o python tranforma o conteúdo em uma
+# tupla por conta da imutabilidade
+
+tupla = ()
