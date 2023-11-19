50d8479538a668c9c9e590743a999881f80136d5
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-19 16:06:05 -0300

    Aprendendo conjuntos "set's"

---
+++ aula11.17/set.py
@@ -0,0 +1,11 @@
+# Set - conjuntos
+
+set01 = set('Paulo')
+set02 = {'Paulo', 'Junior', 'Lara', 'Junior', 'Kaynan', 'Junior', 'Felipe', 'Junior'}
+print(set01)
+print(set02)
+
+lista = [1,2,3,4,5,5,5,5,5,5]
+print(lista)
+set03 = set(lista)
+print(set03)
