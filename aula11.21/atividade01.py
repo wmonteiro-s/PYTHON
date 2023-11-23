5afaa2d95930547a2a7f9d06e872810c8a534e07
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-23 00:49:37 -0300

    Atividades sobre o "for"

---
+++ aula11.21/atividade01.py
@@ -0,0 +1,4 @@
+lista = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+
+for semana in lista:
+    print(semana)
---
+++ aula11.21/atividade02.py
@@ -0,0 +1,4 @@
+for i in range(1, 1000000):
+    print(i)
+    if i % 100 == 0:
+        print(f'{i} é um múltiplo de 100')
