7fea79f3f695444003098be6cb1aa1760b98b4ce
Wesley Monteiro <wmonteiro873@gmail.com>
2023-11-27 20:10:32 -0300

    Funções e import

---
+++ aula11.27/funcoes.py
@@ -0,0 +1,7 @@
+# funções são blocos de códigos que são executados somente quando são chamados
+# parametro: def
+# as funções devem ter prioridade no código
+
+def media(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) / 3
+    return media
---
+++ aula11.27/importando.py
@@ -0,0 +1,5 @@
+from funcoes import media
+n1 = int(input("Digite: "))
+n2 = int(input("Digite: "))
+n3 = int(input("Digite: "))
+print(media(n1,n2,n3))
