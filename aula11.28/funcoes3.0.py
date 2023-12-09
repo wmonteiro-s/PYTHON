a76e3afea553aa7d373f87b58048f18466fbabd1
Wesley Monteiro <wmonteiro873@gmail.com>
2023-12-09 20:37:46 -0300

    aula sobre funções

--- aula11.28/funcoes3.0.py
+++ aula11.28/funcoes3.0.py
@@ -1,8 +1,7 @@
 # Faça uma função que informe a quantidade de dígitos de um determinado número inteiro
 
-def quantidade():
-    numero = input("Digite um número: ")
+def quantidade(numero):
     return len(numero)
 
-print(type(quantidade()))
+numero = input("Digite um número: ")
 print(quantidade())
